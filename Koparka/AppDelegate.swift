8027a28fc27708f410c5eb6d9655a54ef75c96ba
sembsa <sembsa@me.com>
2018-02-12 18:21:33 +0100

    Initial Commit

---
+++ Koparka/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Koparka
+//
+//  Created by Sebastian Trześniewski on 12.02.2018.
+//  Copyright © 2018 Sebastian Trześniewski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Koparka/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Koparka
+//
+//  Created by Sebastian Trześniewski on 12.02.2018.
+//  Copyright © 2018 Sebastian Trześniewski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
